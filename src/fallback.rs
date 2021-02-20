abc5024b921e01ce73b62d5e4c93a39a2d11c0d6
Ben Aubin <ben@benaubin.com>
2021-02-20 17:06:15 -0600

    Rely on rust/llvm's smart iterators, after testing with godbolt

--- src/fallback.rs
+++ src/fallback.rs
@@ -1,33 +1,28 @@
+#[inline]
+fn find_in(needle: u8, haystack: &[u8], len: usize) -> Option<usize> {
+    let idx = haystack.iter().position(|el| *el == needle)?;
+    if len > idx {
+        Some(idx)
+    } else {
+        None
+    }
+}
+
 /// Returns the index of the first element in the haystack that matches needle
 #[inline]
-pub fn find_in_4(needle: u8, haystack: &[u8; 4]) -> usize {
-    if haystack[0] == needle {
-        0
-    } else if haystack[1] == needle {
-        1
-    } else if haystack[2] == needle {
-        2
-    } else if haystack[3] == needle {
-        3
-    } else {
-        4
-    }
+pub fn find_in_4(needle: u8, haystack: &[u8; 4], len: usize) -> Option<usize> {
+    find_in(needle, haystack, len)
 }
 
-pub fn find_in_16(needle: u8, haystack: &[u8; 16]) -> usize {
-    // this implementation should help convince llvm to unroll
-    let mut pos = 0;
-    while haystack[pos] != needle && pos < 16 {
-        pos += 1;
-    }
-    pos
+pub fn find_in_16(needle: u8, haystack: &[u8; 16], len: usize) -> Option<usize> {
+    find_in(needle, haystack, len)
 }
 
-pub fn find_in_32(needle: u8, haystack: &[u8; 32]) -> usize {
-    // this implementation should help convince llvm to unroll
-    let mut pos = 0;
-    while haystack[pos] != needle && pos < 32 {
-        pos += 1;
+pub fn find_in_32(needle: u8, haystack: &[u8; 32], len: usize) -> Option<usize> {
+    find_in(needle, haystack, len)
+}
+
+
+
     }
-    pos
 }
