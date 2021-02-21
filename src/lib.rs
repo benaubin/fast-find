03ab8d2e92b3099213a67d730d0034db7704459b
Ben Aubin <ben@benaubin.com>
2021-02-21 14:10:33 -0600

    refactor to expose fast-find as trait

--- src/lib.rs
+++ src/lib.rs
@@ -13,80 +13,88 @@ mod arch;
 
 mod fallback;
 
-/// Returns the index of the first element in the haystack that matches needle, up-to len, or None
-#[inline]
-pub fn find_in_4(needle: u8, haystack: &[u8; 4], len: usize) -> Option<usize> {
-    arch::find_in_4(needle, haystack, len)
+
+pub trait FastFind {
+    /// Returns the index of the first element in the haystack that matches needle, up-to len, or None
+    fn fast_find(&self, needle: u8, len: usize) -> Option<usize>;
 }
 
-/// Returns the index of the first element in the haystack that matches needle, up-to len, or None
-#[inline]
-pub fn find_in_16(needle: u8, haystack: &[u8; 16], len: usize) -> Option<usize> {
-    arch::find_in_16(needle, haystack, len)
+impl FastFind for [u8; 4] {
+    #[inline]
+    fn fast_find(&self, needle: u8, len: usize) -> Option<usize> {
+        arch::find_in_4(needle, self, len)
+    }
 }
 
-/// Returns the index of the first element in the haystack that matches needle, up-to len, or None
-#[inline]
-pub fn find_in_32(needle: u8, haystack: &[u8; 32], len: usize) -> Option<usize> {
-    arch::find_in_32(needle, haystack, len)
+impl FastFind for [u8; 16] {
+    fn fast_find(&self, needle: u8, len: usize) -> Option<usize> {
+        arch::find_in_16(needle, self, len)
+    }
+}
+
+impl FastFind for [u8; 32] {
+    #[inline]
+    fn fast_find(&self, needle: u8, len: usize) -> Option<usize> {
+        arch::find_in_32(needle, self, len)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::FastFind;
 
     #[test]
     fn test_find_4() {
         let mut array = [0; 4];
 
-        assert_eq!(find_in_4(4, &array, 4), None);
+        assert_eq!(array.fast_find(4, 4), None);
 
         array[1] = 4;
         array[3] = 10;
 
-        assert_eq!(find_in_4(4, &array, 4), Some(1));
-        assert_eq!(find_in_4(5, &array, 4), None);
-        assert_eq!(find_in_4(10, &array, 4), Some(3));
+        assert_eq!(array.fast_find(4, 4), Some(1));
+        assert_eq!(array.fast_find(5, 4), None);
+        assert_eq!(array.fast_find(10, 4), Some(3));
 
-        assert_eq!(find_in_4(4, &array, 2), Some(1));
-        assert_eq!(find_in_4(5, &array, 2), None);
-        assert_eq!(find_in_4(10, &array, 2), None);
+        assert_eq!(array.fast_find(4, 2), Some(1));
+        assert_eq!(array.fast_find(5, 2), None);
+        assert_eq!(array.fast_find(10, 2), None);
     }
 
     #[test]
     fn test_find_16() {
         let mut array = [0; 16];
 
-        assert_eq!(find_in_16(4, &array, 16), None);
+        assert_eq!(array.fast_find(4, 16), None);
 
         array[4] = 4;
         array[6] = 10;
 
-        assert_eq!(find_in_16(4, &array, 16), Some(4));
-        assert_eq!(find_in_16(5, &array, 16), None);
-        assert_eq!(find_in_16(10, &array, 16), Some(6));
+        assert_eq!(array.fast_find(4, 16), Some(4));
+        assert_eq!(array.fast_find(5, 16), None);
+        assert_eq!(array.fast_find(10, 16), Some(6));
 
-        assert_eq!(find_in_16(4, &array, 5), Some(4));
-        assert_eq!(find_in_16(5, &array, 5), None);
-        assert_eq!(find_in_16(10, &array, 5), None);
+        assert_eq!(array.fast_find(4, 5), Some(4));
+        assert_eq!(array.fast_find(5, 5), None);
+        assert_eq!(array.fast_find(10, 5), None);
     }
 
     #[test]
     fn test_find_32() {
         let mut array = [0; 32];
 
-        assert_eq!(find_in_32(4, &array, 16), None);
+        assert_eq!(array.fast_find(4, 16), None);
 
         array[4] = 4;
         array[6] = 10;
         array[20] = 5;
 
-        assert_eq!(find_in_32(4, &array, 32), Some(4));
-        assert_eq!(find_in_32(5, &array, 32), Some(20));
-        assert_eq!(find_in_32(10, &array, 32), Some(6));
+        assert_eq!(array.fast_find(4, 32), Some(4));
+        assert_eq!(array.fast_find(5, 32), Some(20));
+        assert_eq!(array.fast_find(10, 32), Some(6));
 
-        assert_eq!(find_in_32(4, &array, 5), Some(4));
-        assert_eq!(find_in_32(5, &array, 5), None);
-        assert_eq!(find_in_32(10, &array, 5), None);
+        assert_eq!(array.fast_find(4, 5), Some(4));
+        assert_eq!(array.fast_find(5, 5), None);
+        assert_eq!(array.fast_find(10, 5), None);
     }
 }
