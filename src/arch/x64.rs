03d8a580d1aa7009d9baa80986dd601a5f5ea607
Ben Aubin <ben@benaubin.com>
2021-02-21 13:57:41 -0600

    we can relax find_in_16 to only require sse2

--- src/arch/x64.rs
+++ src/arch/x64.rs
@@ -12,8 +12,8 @@ pub use crate::fallback::find_in_4;
 
 #[inline]
 pub fn find_in_16(needle: u8, haystack: &[u8; 16], len: usize) -> Option<usize> {
-    if is_x86_feature_detected!("avx") {
-        let bitfield = unsafe { avx_eq_16(needle, haystack.as_ptr()) };
+    if is_x86_feature_detected!("sse2") {
+        let bitfield = unsafe { sse2_eq_16(needle, haystack.as_ptr()) };
         find_in_bitfield(bitfield, len)
     } else {
         fallback::find_in_16(needle, haystack, len)
@@ -24,8 +24,8 @@ pub fn find_in_16(needle: u8, haystack: &[u8; 16], len: usize) -> Option<usize> 
 pub fn find_in_32(needle: u8, haystack: &[u8; 32], len: usize) -> Option<usize> {
     let bitfield = if is_x86_feature_detected!("avx2") {
         unsafe { avx2_eq_32(needle, haystack.as_ptr()) }
-    } else if is_x86_feature_detected!("avx") {
-        unsafe { avx_eq_32(needle, haystack.as_ptr()) }
+    } else if is_x86_feature_detected!("sse2") {
+        unsafe { sse2_eq_32(needle, haystack.as_ptr()) }
     } else {
         return fallback::find_in_32(needle, haystack, len);
     };
@@ -34,8 +34,6 @@ pub fn find_in_32(needle: u8, haystack: &[u8; 32], len: usize) -> Option<usize> 
 }
 
 /// Finds the index of the last 1 in the bitfield, up-to len
-///
-/// Combines with avx_find_in methods
 #[inline]
 fn find_in_bitfield(bitfield: i32, len: usize) -> Option<usize> {
     let idx = bitfield.trailing_zeros() as usize;
@@ -48,21 +46,21 @@ fn find_in_bitfield(bitfield: i32, len: usize) -> Option<usize> {
 
 /// Returns a bitfield, where all elements in haystack that are equal to needle = 1, else 0
 ///
-/// Safety: requires avx, 16 byte haystack
+/// Safety: requires sse2, 16 byte haystack
 #[inline]
-unsafe fn avx_eq_16(needle: u8, haystack: *const u8) -> i32 {
-    let haystack = _mm_loadu_si128(haystack as *const __m128i);
-    let needle_vec = _mm_set1_epi8(transmute::<u8, i8>(needle));
-    let eq = _mm_cmpeq_epi8(needle_vec, haystack);
-    _mm_movemask_epi8(eq)
+unsafe fn sse2_eq_16(needle: u8, haystack: *const u8) -> i32 {
+    let haystack = _mm_loadu_si128(haystack as *const __m128i); // SSE2
+    let needle_vec = _mm_set1_epi8(transmute::<u8, i8>(needle)); // SSE2
+    let eq = _mm_cmpeq_epi8(needle_vec, haystack); // SSE2
+    _mm_movemask_epi8(eq) // SSE2
 }
 
 /// Returns a bitfield, where all elements in haystack that are equal to needle = 1, else 0
 ///
-/// Safety: requires avx, 32 byte haystack
+/// Safety: requires sse2, 32 byte haystack
 #[inline]
-unsafe fn avx_eq_32(needle: u8, haystack: *const u8) -> i32 {
-    avx_eq_16(needle, haystack) | (avx_eq_16(needle, haystack.offset(16)) << 16)
+unsafe fn sse2_eq_32(needle: u8, haystack: *const u8) -> i32 {
+    sse2_eq_16(needle, haystack) | (sse2_eq_16(needle, haystack.offset(16)) << 16)
 }
 
 /// Returns a bitfield, where all elements in haystack that are equal to needle = 1, else 0
@@ -84,7 +82,7 @@ mod tests {
     fn test_avx_find_16() {
         fn test_all(needle: u8, haystack: &[u8; 16], len: usize, expected: Option<usize>) {
             assert_eq!(
-                find_in_bitfield(unsafe { avx_eq_16(needle, haystack.as_ptr()) }, len),
+                find_in_bitfield(unsafe { sse2_eq_16(needle, haystack.as_ptr()) }, len),
                 expected
             );
             assert_eq!(find_in_16(needle, haystack, len), expected);
@@ -114,7 +112,7 @@ mod tests {
                 expected
             );
             assert_eq!(
-                find_in_bitfield(unsafe { avx_eq_32(needle, haystack.as_ptr()) }, len),
+                find_in_bitfield(unsafe { sse2_eq_32(needle, haystack.as_ptr()) }, len),
                 expected
             );
             assert_eq!(find_in_32(needle, haystack, len), expected);
